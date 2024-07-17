3679138bad52484225b96440916c1313b948cdb2
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-07-17 06:33:02 +0000

    added miniapp for webview

---
+++ bot/plugins/mailable/mail-view.py
@@ -0,0 +1,15 @@
+from bot import bot, web
+import os
+from utils import strip_script_tags
+from quart import render_template
+
+@web.route('/inbox/<user>/<id>')
+async def inbox(user, id):
+  print(user, id)
+  m = await bot.get_messages(int(user), int(id))
+  file = await m.download()
+  f = open(file, "r")
+  content = f.read()
+  nojs = strip_script_tags(content)
+  os.remove(file)
+  return await render_template("inbox.html" , content = nojs)
