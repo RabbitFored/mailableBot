e3c25153cd6a6bac5e97b6950946e6cb37ce9057
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-07-23 13:57:03 +0000

    change func name

--- bot/plugins/hosting/caprover.py
+++ bot/plugins/hosting/caprover.py
@@ -1,12 +1,14 @@
 import os
-from pyrogram import Client, filters
-from bot.core import filters as fltr
-from bot import logger
+
 import requests
+from pyrogram import Client, filters
+
+from bot import logger
+from bot.core import filters as fltr
 
 
 @Client.on_message(filters.command("deploy", "build") & fltr.group("admin"))
-async def build(client, message):
+async def cap_build(client, message):
     logger.info("redeploying")
     build_token = os.environ.get("caprover_build_token","")
     build_url = f"https://captain.theostrich.eu.org/api/v2/user/apps/webhooks/triggerbuild?namespace=captain&token={build_token}"
