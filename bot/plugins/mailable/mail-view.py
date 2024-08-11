ec1794c2c293ee34e2df1541a4fc398de545d496
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-08-11 10:57:35 +0000

    error handled

--- bot/plugins/mailable/mail-view.py
+++ bot/plugins/mailable/mail-view.py
@@ -1,15 +1,26 @@
-from bot import bot, web
 import os
+import tempfile
+
+import aiofiles
+from quart import Response, render_template
+
+from bot import bot, web
 from bot.core.utils import strip_script_tags
-from quart import render_template
+
 
 @web.route('/inbox/<user>/<id>')
 async def inbox(user, id):
-  m = await bot.get_messages(int(user), int(id))
-  file = await m.download()
-  f = open(file, "r")
-  content = f.read()
-  nojs = strip_script_tags(content)
-  os.remove(file)
-  return await render_template("inbox.html" , content = nojs)
+  media = await bot.get_messages(int(user), int(id))
+  try:
+    with tempfile.TemporaryDirectory(prefix=f"{int(user)}_") as temp_dir:
+      temp_file_path = os.path.join(temp_dir, 'mail')
+      await media.download(temp_file_path)
+      async with aiofiles.open(temp_file_path, 'r') as temp_file:
+        content = temp_file.read()
+        nojs = strip_script_tags(content)
+        await temp_file.close()
+        return await render_template("inbox.html" , content = nojs)
+  except ValueError:
+    await bot.send_message(int(user), "File not found")
+    return Response(status=404)
   
