6c56e8463bd1cc00c6e8e1a23a25054fbac5a58e
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-09-23 08:05:03 +0000

    update

--- bot/plugins/mailable/mail-view.py
+++ bot/plugins/mailable/mail-view.py
@@ -10,8 +10,8 @@ from bot.core.utils import strip_script_tags
 
 @web.route('/inbox/<user>/<id>')
 async def inbox(user, id):
-  media = await bot.get_messages(int(user), int(id))
   try:
+    media = await bot.get_messages(int(user), int(id))
     with tempfile.TemporaryDirectory(prefix=f"{int(user)}_") as temp_dir:
       temp_file_path = os.path.join(temp_dir, 'mail')
       await media.download(temp_file_path)
@@ -20,7 +20,7 @@ async def inbox(user, id):
         nojs = strip_script_tags(content)
         await temp_file.close()
         return await render_template("inbox.html" , content = nojs)
-  except ValueError:
+  except:
     await bot.send_message(int(user), "File not found")
     return "File not found"
   
